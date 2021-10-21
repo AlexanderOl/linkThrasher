427beb79f640f4a689dd18567900fa467beca7d0
AlexanderOl <4l3x4nd3r.0l3yn1k@gmail.com>
2021-10-21 16:21:44 +0300

    dir fix

--- Managers/CacheManager.py
+++ Managers/CacheManager.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 class CacheManager:
     def __init__(self, file_name, domain):
-        self.links_file = f"Results/{file_name}/{domain}.json"
+        self.result_dir = f"Results/{file_name}"
+        self.result_filepath = f"{self.result_dir}/{domain}.json"
 
     @staticmethod
     def clear_all():
@@ -24,18 +25,21 @@ class CacheManager:
                 os.remove(os.path.join(result_path, f))
 
     def get_saved_result(self):
-        if os.path.exists(self.links_file):
-            file = open(self.links_file, 'rb')
+        if os.path.exists(self.result_filepath):
+            file = open(self.result_filepath, 'rb')
             data = pickle.load(file)
             file.close()
             return data
 
     def save_result(self, result, has_final_result=False):
-        file = open(self.links_file, 'ab')
+        if not os.path.exists(self.result_dir):
+            os.makedirs(self.result_dir)
+
+        file = open(self.result_filepath, 'ab')
         pickle.dump(result, file)
         file.close()
 
         if has_final_result and len(result) > 0:
             file = open('Results/Final.txt', 'a')
-            res = f'[{datetime.now().strftime("%H:%M:%S")}]: {self.links_file} found {len(result)} \n'
+            res = f'[{datetime.now().strftime("%H:%M:%S")}]: {self.result_filepath} found {len(result)} \n'
             file.write(res)
