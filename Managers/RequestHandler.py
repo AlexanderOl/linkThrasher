a88d461d7faa74923facc3ed22ff43e54c3e1630
AlexanderOl <4l3x4nd3r.0l3yn1k@gmail.com>
2023-02-02 09:04:49 -0500

    dead hosts added

--- Managers/RequestHandler.py
+++ Managers/RequestHandler.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 import requests
 
 
@@ -5,9 +7,16 @@ class RequestHandler:
     def __init__(self, cookies, headers):
         self._cookies = cookies
         self._headers = headers
+        self._dead_hosts = set()
 
     def handle_request(self, url, post_data=None, except_ssl_action=None, except_ssl_action_args: [] = None,
                        timeout=10):
+        parsed_parts = urlparse(url)
+        base_url = f'{parsed_parts.scheme}://{parsed_parts.netloc}/'
+        if base_url in self._dead_hosts:
+            print(f'{base_url} is dead!')
+            return
+
         try:
             if post_data:
                 response = requests.post(url,
@@ -30,6 +39,7 @@ class RequestHandler:
             if except_ssl_action_args:
                 return except_ssl_action(except_ssl_action_args)
         except requests.exceptions.ConnectionError:
+            self._dead_hosts.add(base_url)
             return
         except Exception as inst:
             print(f'Url ({url}) - Exception: {inst}')
