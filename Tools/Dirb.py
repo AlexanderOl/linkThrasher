d037d7417481496a401165468258d62ff1446d4b
AlexanderOl <4l3x4nd3r.0l3yn1k@gmail.com>
2023-02-07 12:31:08 -0500

    dirb updated

--- Tools/Dirb.py
+++ Tools/Dirb.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 from datetime import datetime
+from threading import Timer
 
 from Managers.CacheManager import CacheManager
 
@@ -12,13 +14,28 @@ class Dirb:
 
         report_lines = self.cache_manager.get_saved_result()
         if not report_lines:
-            command = f"dirb {url} -r -f"
-            stream = os.popen(command)
-            bash_outputs = stream.readlines()
 
-            if len(bash_outputs) == 0 and not url.startswith('https:'):
-                url = url.replace('http:', 'https:')
-                self.check_single_url(url)
-            filtered_output = list(filter(lambda o: 'CODE:2' in o or 'DIRECTORY:' in o, bash_outputs))
-            print(f'[{datetime.now().strftime("%H:%M:%S")}]: Dirb {url} finished. Found {len(filtered_output)}')
-            self.cache_manager.save_result(filtered_output)
+            proc = subprocess.Popen(["dirb", url, "-r", "-f"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            kill_action = lambda process: process.kill()
+            my_timer = Timer(600, kill_action, [proc])
+
+            try:
+                my_timer.start()
+                proc.wait()
+                bash_outputs = proc.stderr.read().decode()
+
+                print(f'({url}) err_message - {bash_outputs}')
+
+                if len(bash_outputs) == 0 and not url.startswith('https:'):
+                    url = url.replace('http:', 'https:')
+                    my_timer.cancel()
+                    self.check_single_url(url)
+
+                filtered_output = list(filter(lambda o: 'CODE:2' in o or 'DIRECTORY:' in o, bash_outputs))
+                print(f'[{datetime.now().strftime("%H:%M:%S")}]: Dirb {url} finished. Found {len(filtered_output)}')
+                self.cache_manager.save_result(filtered_output)
+
+            finally:
+                my_timer.cancel()
+
+
