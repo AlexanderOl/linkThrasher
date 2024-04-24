a9c466e9a31a860abe2c572816a3a68bb19a9951
aaa <4l3x4nd3r.0l3yn1k@gmail.com>
2024-04-24 11:25:39 -0400

    updates

--- Tools/Amass.py
+++ Tools/Amass.py
@@ -24,21 +24,11 @@ class Amass:
             for line in bash_outputs:
                 encoded_line = self._ansi_escape.sub('', line)
 
-                if 'node' in encoded_line:
-                    subdomain = encoded_line.split('node')[1].split(' ')[2]
-                    subdomains.add(subdomain)
+                split = encoded_line.split(' ')
+                for item in split:
+                    if self._domain in item:
+                        subdomains.add(item)
 
-                # elif 'contains' in encoded_line and not encoded_line.startswith('10'):
-                #     target = encoded_line.split(' ')[0]
-                #     if '::' in target:
-                #         if int(target.split('/')[1]) > 96:
-                #             subdomains.update(set([str(ip) for ip in ipaddress.IPv6Network(target)]))
-                #     else:
-                #         if int(target.split('/')[1]) > 24:
-                #             subdomains.update(set([str(ip) for ip in ipaddress.IPv4Network(target)]))
-                # if self._domain in encoded_line:
-                #     subdomain = encoded_line.split(' ')[0].replace('\n', '')
-                #     subdomains.add(subdomain)
             cache_manager.save_result(subdomains)
 
         print(f'[{datetime.now().strftime("%H:%M:%S")}]: ({self._domain}) {self._tool_name} found {len(subdomains)} items')
