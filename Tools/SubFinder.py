a29db71b06b9bcdd40da3b4952ae48e6d7cc35de
aaa <4l3x4nd3r.0l3yn1k@gmail.com>
2024-07-01 06:54:57 -0400

    updates

--- Tools/SubFinder.py
+++ Tools/SubFinder.py
@@ -6,11 +6,11 @@ from Helpers.CacheHelper import CacheHelper
 
 class SubFinder:
     def __init__(self, domain):
-        self.__tool_name = self.__class__.__name__
+        self._tool_name = self.__class__.__name__
         self._domain = domain
 
     def get_subdomains(self, avoid_cache=False) -> set:
-        cache_manager = CacheHelper(self.__tool_name, self._domain)
+        cache_manager = CacheHelper(self._tool_name, self._domain)
         subdomains = cache_manager.get_saved_result()
         if (not subdomains and not isinstance(subdomains, set)) or avoid_cache:
             subdomains = set()
@@ -25,5 +25,5 @@ class SubFinder:
 
             cache_manager.cache_result(subdomains)
 
-        print(f'[{datetime.now().strftime("%H:%M:%S")}]: ({self._domain}) {self.__tool_name} found {len(subdomains)} items')
+        print(f'[{datetime.now().strftime("%H:%M:%S")}]: ({self._domain}) {self._tool_name} found {len(subdomains)} items')
         return subdomains
